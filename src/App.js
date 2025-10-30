1d804e11b4957344f923dd9c22a9fef8020a3c85
jeetratanpara25 <jeetratanpara1525@gmail.com>
2025-10-30 01:14:38 +0100

    Testing Vercel auto-deployment

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>DevOps Project with CI/CD Pipeline! 🚀</p>
-<p>Deployed to Vercel!</p>
-<a
+        <p style={{color: '#61dafb'}}>🎉 AUTO-DEPLOYED FROM GITHUB! 🎉</p>
+        
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
